4560b49c19013f7d001072ad26594742ba6055fb
Brandon Corfman <bcorfman@fastmail.fm>
2021-07-19 23:16:06 -0400

    FIX: Update level module to incorporate search module refactor.

--- game/level.py
+++ game/level.py
@@ -1,6 +1,6 @@
 from .room import Room
-from .util import location_ordering, ROW_LENGTH, Perimeter
-from .search import depth_first_search, BlueprintSearchProblem
+from .util import location_ordering, ROW_LENGTH, Perimeter, Stack
+from .search import graph_search, BlueprintSearchProblem
 from .chartypes import ROOM_CHAR, HALLWAY_CHAR
 
 
@@ -34,7 +34,7 @@ class Level:
             for col in range(col_length):
                 if r_item[col:col + 4].upper() == 'ELEV':
                     problem = BlueprintSearchProblem(self.layout, (row, col - 2), ROOM_CHAR)
-                    results = depth_first_search(problem)
+                    results = graph_search(problem, Stack())
                     p = Perimeter(min(results.visited, key=location_ordering),
                                   max(results.visited, key=location_ordering))
                     exits = self._identify_room_exits(p)
@@ -76,13 +76,13 @@ class Level:
         # pick first exit of each elevator as a starting point for search
         for elevator in self.elevators:
             problem = BlueprintSearchProblem(self.layout, elevator.exits[0], HALLWAY_CHAR)
-            results = depth_first_search(problem, return_outliers=True)
+            results = graph_search(problem, Stack())
             locations, room_entrances = results.visited, results.entrances
             self.hallways.extend(results.hallways)
             # Collect information on each new room. Don't consider rooms that have already been found.
             for entrance in room_entrances:
                 problem = BlueprintSearchProblem(self.layout, entrance, ROOM_CHAR)
-                results = depth_first_search(problem)
+                results = graph_search(problem, Stack())
                 p = Perimeter(min(results.visited, key=location_ordering),
                               max(results.visited, key=location_ordering))
                 if p not in perimeters_found:
