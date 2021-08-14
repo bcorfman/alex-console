6c853d3e98ed60b14555d2943c048242b6cdf012
Brandon Corfman <bcorfman@fastmail.fm>
2021-08-13 21:35:06 -0400

    FEATURE: added sample left button mouse events that needs expanding for search next.

--- game/display.py
+++ game/display.py
@@ -1,28 +1,35 @@
+from blessed import Terminal
+from .level import Level
 from .chartypes import ROOM_CHAR, HALLWAY_CHAR, PLAYER_CHARS
 
 
 class Console:
-    def __init__(self, level, terminal):
+    def __init__(self, level: Level, terminal: Terminal):
         self.term = terminal
         self.level = level
         self.t = 0
-        self.display()
 
     def display(self):
-        print(self.term.clear)
+        print(self.term.clear, end='', flush=True)
         for r, row in enumerate(self.level.layout):
             for c, col in enumerate(row):
                 self.term.location(c, r)  # i.e. location(x, y)
                 ch = self.level.layout[r][c]
                 if ch == ROOM_CHAR:
-                    print(self.term.tan_reverse(' '), end='')
+                    print(self.term.tan_reverse(' '), end='', flush=True)
                 elif ch == HALLWAY_CHAR:
-                    print(self.term.white_reverse(' '), end='')
+                    print(self.term.white_reverse(' '), end='', flush=True)
                 elif ch in PLAYER_CHARS:
-                    print(self.term.white_reverse('\u26AB'), end='')
+                    print(self.term.white_reverse('\u26AB'), end='', flush=True)
                 else:
-                    print(ch, end='')
+                    print(ch, end='', flush=True)
+        return True
 
-    def update(self):
-        print(self.term.home + str(self.t))
-        self.t += 1
+    def update(self, row, col, left_button_pressed):
+        if left_button_pressed:
+            # move players according to actions
+            count = str(self.t)
+            length = len(count)
+            print(self.term.move_xy(col, row) + count + self.term.move_left(length), end='', flush=True)
+            self.t += 1
+        return True
--- main.py
+++ main.py
@@ -1,33 +1,61 @@
 import asyncio
+import curses
+from concurrent.futures import ProcessPoolExecutor
 from blessed import Terminal
 from game.util import LEVEL1
 from game.level import Level
 from game.display import Console
+from game.search import BlueprintSearchProblem, graph_search
 
 GAME_TICKS_PER_SECOND = 20
 GAME_TICK = 1.0 / GAME_TICKS_PER_SECOND
+
+
+class LeftButtonPressed:
+    def __init__(self):
+        self.scr = curses.initscr()
+        curses.noecho()
+        curses.cbreak()
+        self.scr.nodelay(True)
+        self.scr.keypad(True)
+        curses.mousemask(curses.BUTTON1_PRESSED)
+        self.scr.refresh()  # TRICKY: without a refresh here, then the first getch() call will erase the screen
+
+    def __enter__(self):
+        return self.scr
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        curses.nocbreak()
+        self.scr.keypad(False)
+        self.scr.nodelay(False)
+        curses.echo()
+        curses.endwin()
 
 
 class Game:
     def __init__(self):
         self.term = Terminal()
         self.console = Console(Level(LEVEL1), self.term)
-        self.loop = asyncio.get_event_loop()
 
-    async def event_loop(self):
-        with self.term.hidden_cursor(), self.term.cbreak():
-            while True:
-                key = self.term.inkey(timeout=0.00)
-                if key.code == self.term.KEY_ESCAPE:
-                    break
-                await asyncio.sleep(GAME_TICK)
-                self.console.update()
-        self.term.move_yx(25, 0)
+    async def event_loop(self, scr):
+        row, col = None, None
+        self.console.display()
+        while True:
+            pressed = False
+            c = scr.getch()
+            if c == self.term.KEY_MOUSE:
+                pressed = True
+                _, col, row, _, _ = curses.getmouse()
+            elif c == ord('q'):
+                break
+            await asyncio.sleep(GAME_TICK)
+            self.console.update(row, col, pressed)
 
 
 async def main():
-    game = Game()
-    await game.event_loop()
+    with LeftButtonPressed() as scr:
+        game = Game()
+        await game.event_loop(scr)
 
 
 if __name__ == '__main__':
