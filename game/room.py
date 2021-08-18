dae6c67a82671d2b76f5e3ac1f2bcb3465f92032
Brandon Corfman <bcorfman@fastmail.fm>
2021-08-17 23:58:10 -0400

    FEATURE: Added test for player in and out of room.

--- game/room.py
+++ game/room.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass, field
 from .util import Perimeter
+from .player import Player
 
 
 @dataclass
@@ -7,3 +8,8 @@ class Room:
     name: str
     perimeter: Perimeter
     exits: list[tuple] = field(default_factory=list)
+
+    def contains(self, player: Player):
+        tl_row, tl_col = self.perimeter.top_left.state
+        br_row, br_col = self.perimeter.bottom_right.state
+        return tl_col <= player.location.col <= br_col and tl_row <= player.location.row <= br_row
--- tests/test_room.py
+++ tests/test_room.py
@@ -1,5 +1,5 @@
 from game.level import Level
-from game.util import LEVEL1
+from game.util import LEVEL1, Loc
 
 
 def test_elevator_exits():
@@ -19,3 +19,39 @@ def test_find_rooms():
     level._find_elevators()
     level._find_rooms()
     assert any(r for r in level.rooms if r.name == 'CARGO' and len(r.exits) == 2)
+
+
+def test_player_not_contained_in_room():
+    level = Level()
+    level._load_layout(LEVEL1)
+    level._add_border_to_layout()
+    level._find_players()
+    level._find_elevators()
+    level._find_rooms()
+    # players on the initial level are in hallways, not in rooms.
+    player = level.get_first_player()
+    player_in_room = False
+    for room in level.rooms:
+        if room.contains(player):
+            player_in_room = True
+            break
+    assert not player_in_room
+
+
+def test_player_contained_in_room():
+    level = Level()
+    level._load_layout(LEVEL1)
+    level._add_border_to_layout()
+    level._find_players()
+    level._find_elevators()
+    level._find_rooms()
+    player = level.get_first_player()
+    # move player's location to the top left corner of the first room.
+    row, col = level.rooms[0].perimeter.top_left.state
+    player.location = Loc(row, col)
+    player_in_room = False
+    for room in level.rooms:
+        if room.contains(player):
+            player_in_room = True
+            break
+    assert player_in_room
