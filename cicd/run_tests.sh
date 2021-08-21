726eabd74311d061682e5c0bc9b742602673666b
Brandon Corfman <bcorfman@fastmail.fm>
2021-08-21 16:48:30 -0400

    FIX: add in call to coveralls module in run_tests.sh. FIX: add coveralls as dependency in tests/requirements.txt

--- cicd/run_tests.sh
+++ cicd/run_tests.sh
@@ -5,4 +5,4 @@ flake8 . --count --select=E9,F63,F7,F82 --show-source --statistics
 flake8 . --count --exit-zero --max-complexity=10 --max-line-length=127 --statistics
 
 coverage run -m pytest
-coverage report
+COVERALLS_REPO_TOKEN=1PDFYWyz8EtFscaGPZxfxZMUPyasLNCTz coveralls
